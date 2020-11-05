f21f37c4162d7eb7f4b559faaf75321c0dfde3c5
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2020-11-06 05:51:18 +0900

    change directory structure

--- bin/build_hterm.sh
+++ bin/build_hterm.sh
@@ -5,12 +5,14 @@ set -Ceu
 HTERM_VERSION='1.89'
 
 CURRENT_DIR=$(cd "$(dirname "$0")" || exit 1; pwd)
+PROJECT_DIR=$(cd "${CURRENT_DIR}/.." || exit 1; pwd)
 TMPDIR=$(mktemp -d)
 trap 'rm -rf "${TMPDIR}"' EXIT
 
 cd "${TMPDIR}" || exit 1
 
 echo "CURRENT_DIR: ${CURRENT_DIR}"
+echo "PROJECT_DIR: ${PROJECT_DIR}"
 echo "TMPDIR: ${TMPDIR}"
 
 git clone 'https://chromium.googlesource.com/apps/libapps'
@@ -19,5 +21,5 @@ git checkout "refs/tags/hterm-${HTERM_VERSION}"
 
 ./hterm/bin/mkdist
 
-mkdir -p "${CURRENT_DIR}/Terminal/Resources"
-cp -f './hterm/dist/js/hterm_all.js' "${CURRENT_DIR}/Terminal/Resources"
+mkdir -p "${PROJECT_DIR}/Resources"
+cp -f './hterm/dist/js/hterm_all.js' "${PROJECT_DIR}/Resources"
